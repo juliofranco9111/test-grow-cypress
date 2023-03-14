c994d928c71859e5f07de3fd8cc69ad44a36668b
Julio Franco <julio.franco9111@gmail.com>
2023-03-14 02:21:45 -0300

    primer commit agrego yml

---
+++ cypress/e2e/test-login.cy.js
@@ -0,0 +1,19 @@
+describe("Test E2E de AuthPage", () => {
+  it("Login Form lleva a Home", () => {
+    Cypress.env("dev", true);
+    const isDev = Cypress.env().dev;
+    const url = isDev
+      ? "http://localhost:5173/login"
+      : "https://grow-react-beta.vercel.app/login";
+
+    const email = Cypress.env().email;
+    const password = Cypress.env().password;
+    cy.visit(url);
+
+    cy.get(':nth-child(1) > input[type="email"]').type(email);
+    cy.get(':nth-child(2) > input[type="password"]').type(password);
+    cy.get('[data-cy="submit"]').click();
+
+    cy.url().should("include", "/home");
+  });
+});
